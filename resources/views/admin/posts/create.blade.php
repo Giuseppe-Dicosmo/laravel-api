999b24ae0cfd0f140fac36badd562adb41ccb3b3
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-05-04 23:33:09 +0200

    .

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -10,6 +10,7 @@
     <div class="form-group">
       <label for="title">Titolo</label>
       <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+
       @error('title')
       <div class="invalid-feedback">{{ $message }}</div>
       @enderror
@@ -18,6 +19,7 @@
     <div class="form-group">
       <label for="content">Testo</label>
       <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="3">{{ old('content') }}</textarea>
+
       @error('content')
       <div class="invalid-feedback">{{ $message }}</div>
       @enderror
@@ -26,7 +28,23 @@
     <div class="form-group">
       <label for="publisheder_at">Data di pubblicazione</label>
       <input type="date" class="form-control @error('publisheder_at') is-invalid @enderror" id="publisheder_at" name="publisheder_at" value="{{ old('publisheder_at') }}">
+
       @error('publisheder_at')
+      <div class="invalid-feedback">{{ $message }}</div>
+      @enderror
+    </div>
+
+    <div class="form-group">
+      <label for="category_id">Categoria</label>
+      <select class="form-control @error('category_id') is-invalid @enderror" id="category_id" name="category_id">
+        <option value="">Seleziona</option>
+        @foreach($categories as $category)
+        <option {{ old('category_id') && old('category_id') == $category->id ? 'selected' : '' }}
+          value="{{ $category->id }}">{{ $category->name }}</option>
+        @endforeach
+      </select>
+
+      @error('category_id')
       <div class="invalid-feedback">{{ $message }}</div>
       @enderror
     </div>
